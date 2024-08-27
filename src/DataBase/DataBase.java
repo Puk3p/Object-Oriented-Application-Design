11a133e4381b2f9801177d72c33b263bc90f0df8
Lupu Gheorghe <117625145+Puk3p@users.noreply.github.com>
2024-08-27 18:35:08 +0300

    Update DataBase.java

--- src/DataBase/DataBase.java
+++ src/DataBase/DataBase.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBase {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/table";
+    private static final String URL = "jdbc:sqlite://127.0.0.1:3306/table";
     private static final String USER = "root";
     private static final String PASSWORD = "luigiteiubesc";
 
